56ce244203ee637e54fbdf2e2e706abc1943378e
qzwang518 <qinzheng@usc.edu>
2024-02-03 15:31:10 -0800

    second commit

--- middleware.ts
+++ middleware.ts
@@ -6,15 +6,15 @@ export default authMiddleware({
         '/events/:id',
         '/api/webhook/clerk',
         '/api/webhook/stripe',
-        '/api/uploadthing',
+        '/api/uploadthing'
     ],
     ignoredRoutes: [
         '/api/webhook/clerk',
         '/api/webhook/stripe',
-        '/api/uploadthing',
+        '/api/uploadthing'
     ]
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
